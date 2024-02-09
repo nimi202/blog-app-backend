c8360e0b20a76010ddd3baca24c0fc5f8193dd84
nimi202 <nimiharison2002@gmail.com>
2024-02-09 11:33:14 +0530

    pass

--- controllers/blogRouter.js
+++ controllers/blogRouter.js
@@ -1,14 +1,31 @@
-const express=require("express")
-const blogModel=require("../models/blogModels")
+const express = require("express")
+const blogModel = require("../models/blogModels")
 
-const router=express.Router()
-router.post("/add",async(req,res)=>{
-    let data= req.body
-    let users=new blogModel(data)
-    let result=await users.save()
-    res.json(
-        {
-        status:"success"
-        })
+const router = express.Router()
+const bcrypt = require("bcryptjs")
+
+hashPasswordGenerator = async (pass) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass, salt)
+}
+router.post("/add", async (req, res) => {
+    let { data } = { "data": req.body }
+    let password = data.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword) => {
+            console.log(hashedPassword)
+            data.password = hashedPassword
+            console.log(data)
+            let users = new blogModel(data)
+            let result =  users.save()
+            res.json(
+                {
+                    status: "success"
+                })
+        }
+    )
+
+
+    
 })
-module.exports=router
+module.exports = router
